8377cbe4a5d1044e5781388483935a1fbd0c56aa
shuaizhang621 <sz1950@nyu.edu>
2018-04-19 19:30:29 -0400

    search bar stage 2 & move tabs up

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Icon, Input, AutoComplete } from 'antd';
+import { Icon, Input, AutoComplete, Radio} from 'antd';
 import {ItemContainer} from "./ItemContainer";
+import { Select } from 'antd';
+
 const Option = AutoComplete.Option;
 const OptGroup = AutoComplete.OptGroup;
+const Search = Input.Search;
 
 export class SearchContainer extends React.Component {
     state = {
         searched: false,
-        searchPeople: false,
-        searchJob: false,
-        searchCompany: false,
-        searchAll: false,
-        search: '',
+        searchFor: 'people',
     }
 
     renderTitle = (title) => {
@@ -20,63 +19,67 @@ export class SearchContainer extends React.Component {
         );
     };
 
-    handleSelect = (value) => {
-        console.log(value);
-        if (value == 'companies') {
-            this.setState({
-                searched: true,
-                search: value,
-            });
-        } else if (value == 'people') {
-            this.setState({
-                searched: true,
-                search: value,
-            });
-        } else if (value == 'jobs'){
-            this.setState({
-                searched: true,
-                search: value,
-            });
-        }
-    };
+    // handleSelect = (value) => {
+    //     console.log(value);
+    //     if (value == 'companies') {
+    //         this.setState({
+    //             searched: true,
+    //             search: value,
+    //         });
+    //     } else if (value == 'people') {
+    //         this.setState({
+    //             searched: true,
+    //             search: value,
+    //         });
+    //     } else if (value == 'jobs'){
+    //         this.setState({
+    //             searched: true,
+    //             search: value,
+    //         });
+    //     }
+    // };
+    //
+    // renderSearchContent = () => {
+    //     if (this.state.searched === false) {
+    //         return null;
+    //     } else if (this.state.searchAll === true) {
+    //         return (
+    //             <div>
+    //                 <span>People result:</span>
+    //                 <ItemContainer/>
+    //                 <span>Job result:</span>
+    //                 <ItemContainer/>
+    //                 <span>Company result:</span>
+    //                 <ItemContainer/>
+    //             </div>
+    //         )
+    //     } else if (this.state.searchPeople === true) {
+    //         return (
+    //             <div>
+    //                 <span>People result:</span>
+    //                 <ItemContainer/>
+    //             </div>
+    //         )
+    //     } else if (this.state.searchJob === true) {
+    //         return (
+    //             <div>
+    //                 <span>Job result:</span>
+    //                 <ItemContainer/>
+    //             </div>
+    //         )
+    //     } else if (this.state.searchCompany === true) {
+    //         return (
+    //             <div>
+    //                 <span>Company result:</span>
+    //                 <ItemContainer/>
+    //             </div>
+    //         )
+    //     }
+    // };
 
-    renderSearchContent = () => {
-        if (this.state.searched === false) {
-            return null;
-        } else if (this.state.searchAll === true) {
-            return (
-                <div>
-                    <span>People result:</span>
-                    <ItemContainer/>
-                    <span>Job result:</span>
-                    <ItemContainer/>
-                    <span>Company result:</span>
-                    <ItemContainer/>
-                </div>
-            )
-        } else if (this.state.searchPeople === true) {
-            return (
-                <div>
-                    <span>People result:</span>
-                    <ItemContainer/>
-                </div>
-            )
-        } else if (this.state.searchJob === true) {
-            return (
-                <div>
-                    <span>Job result:</span>
-                    <ItemContainer/>
-                </div>
-            )
-        } else if (this.state.searchCompany === true) {
-            return (
-                <div>
-                    <span>Company result:</span>
-                    <ItemContainer/>
-                </div>
-            )
-        }
-    };
+    handleSearch = (value) => {
+        console.log(value);
+    }
 
     render() {
         const dataSource = [{
@@ -113,23 +116,27 @@ export class SearchContainer extends React.Component {
         ));
 
         return (
-        <div className="certain-category-search-wrapper" style={{ width: 650 }}>
-            <AutoComplete
-                className="certain-category-search"
-                dropdownClassName="certain-category-search-dropdown"
-                dropdownMatchSelectWidth={false}
-                dropdownStyle={{ width: 300 }}
-                size="large"
-                style={{ width: '100%' }}
-                dataSource={options}
-                placeholder="input here"
-                optionLabelProp="value"
-                onSelect={this.handleSelect}
-            >
-                <Input suffix={<Icon type="search" className="certain-category-icon" />} />
-            </AutoComplete>
-            {this.renderSearchContent}
-        </div>
+            <div className="search-bar">
+                <div className="search-selection">
+                    <span class="search-for">Search for:</span>
+                    <Radio.Group className="search-radio" value="people" onChange={this.handleSearch}>
+                        <Radio.Button style={{ width: 175 }} value="large">
+                            <Icon type="team" />People
+                        </Radio.Button>
+                        <Radio.Button style={{ width: 175 }} value="default">
+                            <Icon type="red-envelope" />Jobs
+                        </Radio.Button>
+                        <Radio.Button style={{ width: 175 }} value="small">
+                            <Icon type="shop" />Companies
+                        </Radio.Button>
+                    </Radio.Group>
+                </div>
+                <Search
+                    placeholder="input search text"
+                    onSearch={value => console.log(value)}
+                    style={{ width: 620 }}
+                />
+            </div>
         );
     }
 }
