84d17fdcec0c2b1a7d7e37ecd34189399cdb8efe
shuaizhang621 <sz1950@nyu.edu>
2018-04-30 23:20:30 -0400

    logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import lobster from '../assets/image/lobster.svg';
+import icon from '../assets/image/jobster.png';
 import { Icon } from 'antd';
 //"https://static.licdn.com/sc/h/95o6rrc5ws6mlw6wqzy0xgj7y"
 
@@ -12,7 +13,7 @@ export class Header extends React.Component {
             <header className="App-header">
                 <div className="valid-header">
                     {/*<div className="App-logo">JOBSTER</div>*/}
-                    <img src={jobster} className="App-logo" alt="logo" />
+                    <img src={icon} className="App-logo" alt="logo" />
                     {this.props.isLoggedIn ?
                         <a className = "logout" onClick={this.props.handleLogout}>
                             <Icon type="logout" />{' '}Logout
