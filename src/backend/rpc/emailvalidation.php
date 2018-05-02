4cc88971d2422b4a854ff2108b824c64edf6d1b2
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-05-01 23:32:10 -0400

    no message

--- src/backend/rpc/emailvalidation.php
+++ src/backend/rpc/emailvalidation.php
@@ -2,34 +2,7 @@
 //this script is used to check the validation of the email of registration or check if it has been already used before.
 //Get the parameter from forntend based on wheater it is a student user or a company user.
 $user_type = $_POST['usertype'];
-//testdata
-//already exist
-//$reg_username = 'dx1368@nyu.edu';
-//$user_type = 'student';
-//not exist
-//$reg_username = 'rh1514@nyu.edu';
-//$user_type = 'student';
-if ($user_type == 'student')
-{
-//    echo 'student';
-    $reg_username = $_POST['semail'];
-    $sql_check_double_email = "select semail from Student where semail = ?;";
-    //query to check if there is a semail which is the same as the new one.
-    $double_check  = $conn->prepare($sql_check_double_email);
-    $double_check->bind_param('s',$semail);
-    $double_check->execute();
-    $result_double_check = $double_check->get_result();
-}
-elseif ($user_type == 'company') {
-//    echo 'company';
-    $reg_username =$_POST['cname'];
-    $sql_check_double_email = "select cname from Company where cname = ?;";
-    //query to check if there is a semail which is the same as the new one.
-    $double_check  = $conn->prepare($sql_check_double_email);
-    $double_check->bind_param('s',$cname);
-    $double_check->execute();
-    $result_double_check = $double_check->get_result();
-}
+
 //the parameters that used for connecting to database.
 $servername = "localhost";
 $dbusername = "root";
@@ -41,6 +14,30 @@ $conn = new mysqli($servername, $dbusername, $password, $dbname);
 if ($conn->connect_error) {
     die(json_encode(array('message' => "Connection failed: " . $conn->connect_error)));
 }
+
+
+if ($user_type == 'student')
+{
+//    echo 'student';
+    $reg_username = $_POST['semail'];
+    $sql_check_double_email = "select semail from Student where semail = ?;";
+    //query to check if there is a semail which is the same as the new one.
+    $double_check  = $conn->prepare($sql_check_double_email);
+    $double_check->bind_param('s',$reg_username);
+    $double_check->execute();
+    $result_double_check = $double_check->get_result();
+}
+elseif ($user_type == 'company') {
+//    echo 'company';
+    $reg_username =$_POST['cname'];
+    $sql_check_double_email = "select cname from Company where cname = ?;";
+    //query to check if there is a semail which is the same as the new one.
+    $double_check  = $conn->prepare($sql_check_double_email);
+    $double_check->bind_param('s',$reg_username);
+    $double_check->execute();
+    $result_double_check = $double_check->get_result();
+}
+
 
 //get result of double_check.
 if ($result_double_check->num_rows > 0)
@@ -56,3 +53,4 @@ else
     echo json_encode($response);
 }
 $conn->close();
+?>
